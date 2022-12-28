22d1d9be30ddfd75266a4d1e993cb1fcc804a1ef
BillStephens2022 <two4onebill@yahoo.com>
2022-12-28 12:53:41 -0500

    added comments in server.js file

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
+// import node modules
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
+//import notes json file used for saving notes
 const notes = require('./db/db.json');
 
+// helper to add a random ID to each note object
 const uuid = require('./helpers/uuid');
 
+// definite local host port to use
 const PORT = 3001;
 
+// create instance of Express
 const app = express();
 
+// middleware to be used
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// handles GET requests at home route.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// handles GET requests for 'notes' route.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// handles GET requests for the 'api/notes' route which will display all of the note objects in json format
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+// handles POST requests for new notes added by user.
 app.post('/api/notes', (req, res) => {
     let parsedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
@@ -37,6 +48,7 @@ app.post('/api/notes', (req, res) => {
     res.json(parsedNotes);
 });
 
+// handles request to see specific note objects by unique note ID.
 app.get('/api/notes/:id', (req, res) => {
     if (req.params.id) {
         console.info(`${req.method} request received to get a single note`);
@@ -55,6 +67,7 @@ app.get('/api/notes/:id', (req, res) => {
       }
 });
 
+// handles DELETE requests made by the user to delete specific notes.
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
     let parsedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -72,10 +85,12 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(notes);   
     });
 
+// handles any other GET request made by the user - will automatically display home page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// sets up server to listen on PORT which was defined above as port 3001.
 app.listen(PORT, () => 
     console.log(`Express Server running on http://localhost:${PORT}`)
 );
